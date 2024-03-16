40551a5122ca22bc76c1a496627c51950a7d1716
shiro0114 <sugu01140114@gmail.com>
2024-03-16 22:14:27 +0900

    update task3.js

--- task3.js
+++ task3.js
@@ -1,33 +1,30 @@
-function taskFactory(taskList){
+function viewTask(taskList) {
+    const text = `${'='.repeat(20)}\n現在持っているタスク一覧\n${'='.repeat(20)}`;
+
+    console.log(text);
+
+    for (let i = 0; i < taskList.length; i++) {
+        console.log(`${i} : ${taskList[i]}`);
+    }
+}
+
+function taskFactory(taskList) {
 
     return {
-            viewTask: function(){
-                                const text = [
-                                                '=====================',
-                                                '現在持っているタスク一覧',
-                                                '====================='
-                                            ].join('\n');
-                            
-                                console.log(text);
-                            
-                                for( let i = 0; i < taskList.length; i++){
-                                    console.log(`${i} : ${taskList[i]}`);
-                                }
-                            },
-
-            addTask: function (task){
-                                    taskList.push(task);
-                                    this.viewTask();
-                                }
+        taskList,
+        addTask: function (task) {
+            this.taskList.push(task);
+            viewTask(this.taskList);
+        }
     };
 }
 
-const taskList = taskFactory(['掃除', '買い物', '散歩']);
+const tasks = taskFactory(['掃除', '買い物', '散歩']);
 
-taskList.viewTask();
+viewTask(tasks.taskList);
 
 let userInput = prompt('タスクを入力してください');
 
-taskList.addTask(userInput);
+tasks.addTask(userInput);
 
 userInput = prompt('[確認、追加、削除、終了]の4つのつのいずれかを入力してください');
