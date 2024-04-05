e1ec1b873f9b7c2babf03a5870104dd96967b76e
shiro0114 <sugu01140114@gmail.com>
2024-04-05 22:42:00 +0900

    modify 4/5

--- main.js
+++ main.js
@@ -1,78 +1,51 @@
-class IdController {
-    constructor(id = 0) {
-        this.id = id;
-    };
-    increment = () => this.id++;
-}
+const userInput = document.querySelector('#userInput')
+const addTaskBtn = document.querySelector('#addTaskBtn');
+const output = document.querySelector('#output');
+
+const tasks = new Map;
+let id = 0;
 
 class Task {
-    constructor(comment, condition = '作業中') {
+    constructor(comment, condition) {
         this.comment = comment;
         this.condition = condition;
     }
 }
 
-class TaskMapController {
-    constructor() {
-        this.idController = new IdController();
-        this.taskMap = new Map();
-    }
-    addTask(comment) {
-        const newTask = new Task(comment)
-        this.taskMap.set(this.idController.id, newTask);
-        this.idController.increment();
-    }
+addTableDataText = (value, element) => {
+    const td = document.createElement('td');
+    td.textContent = value;
+    element.appendChild(td);
 }
 
-class AddTableText {
-    addContent(value, element) {
-        const td = document.createElement('td');
-        td.textContent = value
-        element.appendChild(td);
-    }
+addTableDataButton = (value, element) => {
+    const td = document.createElement('td');
+    const btn = document.createElement('button');
+    btn.textContent = value;
+    td.appendChild(btn);
+    element.appendChild(td);
 }
 
-class AddTableButton {
-    addContent(value, element) {
-        const td = document.createElement('td');
-        const btn = document.createElement('button');
-        btn.textContent = value;
-        td.appendChild(btn);
-        element.appendChild(td);
-    }
+viewTask = taskMap => {
+    taskMap.forEach((task, id) => {
+        const tableRow = document.createElement('tr');
+        addTableDataText(id, tableRow);
+        addTableDataText(task.comment, tableRow);
+        addTableDataButton(task.condition, tableRow);
+        addTableDataButton('削除', tableRow);
+        output.append(tableRow);
+    })
 }
-
-class AddRowController {
-    constructor(outputTable) {
-        this.addTableText = new AddTableText();
-        this.addTableButton = new AddTableButton();
-        this.outputTable = outputTable;
-    }
-
-    addRow(taskMap) {
-        taskMap.forEach((task, id) => {
-            const tr = document.createElement('tr');
-            this.addTableText.addContent(id, tr);
-            this.addTableText.addContent(task.comment, tr);
-            this.addTableButton.addContent(task.condition, tr);
-            this.addTableButton.addContent('削除', tr);
-            this.outputTable.appendChild(tr);
-        })
-    }
-}
-
-const currentTask = new TaskMapController();
-const userInput = document.querySelector('#userInput')
-const addTaskBtn = document.querySelector('#addTaskBtn');
-const output = document.querySelector('#output');
-
-
 
 addTaskBtn.onclick = (event) => {
     output.innerHTML = '';
-    currentTask.addTask(userInput.value);
-    const addcontroller = new AddRowController(output);
-    addcontroller.addRow(currentTask.taskMap);
+
+    const newTask = new Task(userInput.value, '作業中');
+    tasks.set(id, newTask);
+    id++;
+
+    viewTask(tasks)
+
     userInput.value = '';
 };
 
