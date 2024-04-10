4c22caae20ff766accbd89f02371e3bf27a72efd
shiro0114 <sugu01140114@gmail.com>
2024-04-10 23:17:09 +0900

    lesson3-2

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const userInput = document.querySelector('#userInput')
+const userInput = document.querySelector('#userInput');
 const addTaskBtn = document.querySelector('#addTaskBtn');
 const output = document.querySelector('#output');
 
-const tasks = new Map;
+let tasks = new Map();
 let id = 0;
 
 class Task {
@@ -16,15 +16,36 @@ const addTableDataText = (value, element) => {
     const tableData = document.createElement('td');
     tableData.textContent = value;
     element.appendChild(tableData);
-}
+};
 
-const addTableDataButton = (value, element) => {
+const addTableDataButton = (value, element, clickHandler) => {
     const tableData = document.createElement('td');
     const btn = document.createElement('button');
     btn.textContent = value;
     tableData.appendChild(btn);
     element.appendChild(tableData);
+    if (clickHandler) btn.onclick = clickHandler;
+};
+
+const clickDelete = event => {
+    const tableRow = event.target.closest('tr');
+    const taskId = Number(tableRow.firstChild.innerHTML);
+    tasks.delete(taskId);
+    resetId(tasks);
+    output.innerHTML = '';
+    viewTask(tasks);
 }
+
+const resetId = taskMap => {
+    id = 0;
+    let tmpTasks = new Map();
+
+    taskMap.forEach((task) => {
+        tmpTasks.set(id++, task);
+    })
+
+    tasks = tmpTasks;
+};
 
 const viewTask = taskMap => {
     taskMap.forEach((task, id) => {
@@ -32,19 +53,18 @@ const viewTask = taskMap => {
         addTableDataText(id, tableRow);
         addTableDataText(task.comment, tableRow);
         addTableDataButton(task.condition, tableRow);
-        addTableDataButton('削除', tableRow);
+        addTableDataButton('削除', tableRow, clickDelete);
         output.appendChild(tableRow);
-    })
-}
+    });
+};
 
-addTaskBtn.onclick = (event) => {
+addTaskBtn.onclick = event => {
     output.innerHTML = '';
 
     const newTask = new Task(userInput.value, '作業中');
     tasks.set(id++, newTask);
 
-    viewTask(tasks)
+    viewTask(tasks);
 
     userInput.value = '';
 };
-
